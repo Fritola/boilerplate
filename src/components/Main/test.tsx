009757f1a23b70fa71014a64fd15b40a523c68ac
Fritola <gustavo.silva@ab-inbev.com>
2022-11-30 10:52:35 -0300

    colors test

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -11,7 +11,7 @@ describe('<Main />', () => {
     expect(container.firstChild).toMatchSnapshot()
   })
 
-  it('should render color correctly', () => {
+  it('should render colors correctly', () => {
     const { container } = render(<Main />)
     expect(container.firstChild).toHaveStyle({
       'background-color': '#06092b'
